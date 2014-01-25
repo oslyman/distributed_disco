13ae05b32860e51800d19001a5c69c7a07668d33
Oliver Lyman <Oliver@Oliver-Lymans-MacBook-Pro.local>
2014-01-24 23:03:07 -0500

    Removed some js that is no longer needed

--- hoster.js
+++ hoster.js
@@ -1,41 +1,7 @@
 document.getElementById("play").onclick=function(){
 	//document.getElementById("main").innerHTML="New text!";
-	document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
-	
-}
-
-
-var context;
-window.addEventListener('load', init, false);
-
-function init() {
-	try {
-		window.AudioContext = window.AudioContext||window.webkitAudioContext;
-		context = new AudioContext();
-	}
-	catch(e) {
-		alert('Web Audio API is not supported in this browser');
-	}
-}
-
-var songBuffer = null;
-
-function loadSong(url) {
-	var request = new XMLHttpRequest();
-	request.open('GET', url, false);
-	request.responseType = 'arraybuffer';
-
-	request.onload = function() {
-		context.decodeAudioData(request.response, function(buffer) {
-			songBuffer = buffer;
-		}, onError);
-	}
-	request.send();
-}
-
-function playSong(buffer) {
-	var source = context.createBufferSource();
-	source.buffer = buffer;
-	source.connect(context.destination);
-	source.start();
+	//document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
+	init();
+	loadSong("music/rebel.m4a");
+	playSong(songBuffer);
 }
