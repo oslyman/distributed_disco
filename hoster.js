37a976514b2495dace5427c4bef5dbb6526a35c5
Jason Hu <hu@hu.local>
2014-01-25 04:42:08 -0500

    Trying to make it pretttty

--- hoster.js
+++ hoster.js
@@ -3,12 +3,14 @@ var names = ["Rebel Rebel - David Bowie", "Rebellion - Arcade Fire", "You Make M
 function init(){
 	newtext = "Pick a song to play!\n"
 	for (var i = 0; i<songs.length; i++){
-		newtext += "<p id=" + i + ">" + names[i] + "</p>";
+		var name = names[i];
+		var song = songs[i];
+		newtext += "<a href=\"hoster?song=" + song.slice(0, song.indexOf(".")) + "&format=" + song.slice(song.indexOf(".") + 1, song.length) + "\"><p id=" + i + ">" + names[i] + "</p></a>";
 	}
 	document.getElementById("main").innerHTML=newtext;
-	for(var i = 0; i<songs.length; i++){
-		document.getElementById(i).onclick=gen_fun(songs[i])
-	}
+	// for(var i = 0; i<songs.length; i++){
+	// 	document.getElementById(i).onclick=gen_fun(songs[i])
+	// }
 }
 
 function gen_fun(name){
--- stream.js
+++ stream.js
@@ -1,36 +1,72 @@
-var	http = require('http'),
-	server = http.createServer(),
+var	express = require('express'),
+	routes = require('./routes'),
+	path = require('path'),
+	app = express(),
+	http = require('http'),
+	server = http.createServer(app),
 	url = require('url'),
 	fs = require("fs"),
-	stream,
+	// stream,
 	lame = require("lame"),
 	Speaker = require("speaker"),
 	decoder = new lame.Decoder(),
 	encoder = new lame.Encoder({channels: 2, bitDepth:16, sampleRate:44100});
 
-server.on('request', handleRequest);
-server.listen(8000);
+// all environments
+app.set('port', process.env.PORT || 8000);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'jade');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
 
-var clients = [];
+// development only
+// if ('development' == app.get('env')) {
+//   app.use(express.errorHandler());
+// }
+
+app.get('/', routes.index);
+
+server.listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// server.on('request', handleRequest);
+// server.listen(8000);
+
+var clients = [],
+	stream;
 
 // encoder.on("data", function(data){
 // 	sendData(data);
 // });
 
-function handleRequest(req, res){
-	console.log("HELLO");
+function changeHoster(req, res){
 	res.writeHead(200, {'content-type': 'text/html'});
-	res.write("HI");
+	var query = url.parse(req.url, true).query;
+	stream = fs.createReadStream("music/" +  query.song+ "." + query.format).pipe(decoder);
+	res.end();
+}
+
+function addJoiner(req, res){
+	res.writeHead(200, {'content-type': 'text/html'});
+	res.speaker = new Speaker();
+	stream.pipe(res.speaker);
+	clients.push(res);
+	res.end();
+}
+
+function handleRequest(req, res){
 	var requestURL = url.parse(req.url, true),
 		path = requestURL.pathname,
 		query = requestURL.query;
-	console.log(path);
 	if (path=="/hoster"){
 		console.log("I'm a hoster!");
 		stream = fs.createReadStream("music/" +  query.song+ "." + query.format).pipe(decoder);
-		// stream.on("data", function(data){
-		// 	encoder.write(data);
-		// });
 	}
 	else if (path=="/joiner"){
 		res.speaker = new Speaker();
@@ -43,14 +79,4 @@ function handleRequest(req, res){
 	}
 
 	res.end();
-}
-
-// function sendData(data){
-// 	clients.forEach(function(client){
-// 		client.write(data);
-// 		decoder.pipe(client.)
-// 	});
-// }
-function playSong(songName){
-	stream.pipe(decoder).pipe(speaker);
 }
