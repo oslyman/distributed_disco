5acee6cc16bf85286fba169a888be0d2d35686cf
Oliver Lyman <Oliver@Oliver-Lymans-MacBook-Pro.local>
2014-01-24 23:04:09 -0500

    Typo

--- hoster.js
+++ hoster.js
@@ -1,7 +1,4 @@
 document.getElementById("play").onclick=function(){
 	//document.getElementById("main").innerHTML="New text!";
-	//document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
-	init();
-	loadSong("music/rebel.m4a");
-	playSong(songBuffer);
+	document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
 }
