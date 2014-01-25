e51a241a12b275819bb00fb7ff49b5db3bfd2a31
Miles Eldon <mileseldon@yahoo.com>
2014-01-24 22:01:10 -0500

    update

--- hoster.js
+++ hoster.js
@@ -1,7 +1,5 @@
 document.getElementById("play").onclick=function(){
 	//document.getElementById("main").innerHTML="New text!";
-	
-	
-	document.getElementById("main").innerHTML="<embed src=\"music/rebel.m4a\" loop=\"true\" autoplay=\"true\" width=\"0\" height=\"0\" </embed>"
+	document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
 	
 }
