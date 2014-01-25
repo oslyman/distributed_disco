ceafb887a3895d039f9e9b530c8d7d3d8105133f
Miles Eldon <mileseldon@yahoo.com>
2014-01-24 23:39:05 -0500

    woooo hoster works like a sexy sexy sexy something sexy

--- hoster.js
+++ hoster.js
@@ -1,4 +1,19 @@
-document.getElementById("play").onclick=function(){
-	//document.getElementById("main").innerHTML="New text!";
-	document.getElementById("main").innerHTML="<embed height=\"15\" width=\"300\" src=\"music/rebel.m4a\">"
+var songs = ["rebel.m4a", "rebellion.mp3", "youmakeme.m4a", "zeggie.mp3"];
+var names = ["Rebel Rebel - David Bowie", "Rebellion - Arcade Fire", "You Make Me Feel - Cobra Starship", "Zeggie - Afrojack"]
+function init(){
+	newtext = "Pick a song to play!\n"
+	for (var i = 0; i<songs.length; i++){
+		newtext += "<p id=" + i + ">" + names[i] + "</p>";
+	}
+	document.getElementById("main").innerHTML=newtext;
+	for(var i = 0; i<songs.length; i++){
+		document.getElementById(i).onclick=gen_fun(songs[i])
+	}
 }
+
+function gen_fun(name){
+	return function(){
+			document.getElementById("player").innerHTML="<embed height=\"15\" width=\"300\" autoplay=\"true\" src=\"music/" + name+ "\">";
+		}
+}
+init()
