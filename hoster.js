fd18bcd9565bb29b675c403d905a210929654d33
Miles Eldon <mileseldon@yahoo.com>
2014-01-24 21:11:03 -0500

    music (functional on firefox

---
+++ hoster.js
@@ -0,0 +1,7 @@
+document.getElementById("play").onclick=function(){
+	//document.getElementById("main").innerHTML="New text!";
+	
+	
+	document.getElementById("main").innerHTML="<embed src=\"music/rebel.m4a\" loop=\"true\" autoplay=\"true\" width=\"0\" height=\"0\" </embed>"
+	
+}
